d4393a7a623410ebeeef3e2fb79206f42b0e5001
adwait1998 <43320637+adwait1998@users.noreply.github.com>
2021-11-27 20:26:15 -0700

    Update Cell.java
    US#1  T#11 Created a cell model

--- src/view/Cell.java
+++ src/view/Cell.java
@@ -3,4 +3,11 @@ package view;
 import javax.swing.*;
 
 public class Cell extends JButton {
+    public final int x;
+    public final int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
